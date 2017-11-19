179080211f789a938f4aa53420b42607d679ab0f
jbjouvin <jb.jouvin@gmail.com>
2017-11-19 15:23:08 +0100

    + message tests

--- e2e/index.test.js
+++ e2e/index.test.js
@@ -11,11 +11,12 @@ test(`users should be able to view the '/' page`, async t => {
 });
 
 test(`should display the page correctly if a user is not logged in`, async (t) => {
-    await t
-      .navigateTo(TEST_URL)
-      .expect(Selector('H1').withText('All Users').exists).ok()
-      .expect(Selector('a').withText('User Status').exists).notOk()
-      .expect(Selector('a').withText('Log Out').exists).notOk()
-      .expect(Selector('a').withText('Register').exists).ok()
-      .expect(Selector('a').withText('Log In').exists).ok()
-  });
+  await t
+    .navigateTo(TEST_URL)
+    .expect(Selector('H1').withText('All Users').exists).ok()
+    .expect(Selector('a').withText('User Status').exists).notOk()
+    .expect(Selector('a').withText('Log Out').exists).notOk()
+    .expect(Selector('a').withText('Register').exists).ok()
+    .expect(Selector('a').withText('Log In').exists).ok()
+    .expect(Selector('.alert').exists).notOk()
+});
--- e2e/login.test.js
+++ e2e/login.test.js
@@ -48,26 +48,18 @@ test(`should allow a user to sign in`, async t => {
     .typeText('input[name="password"]', password)
     .click(Selector('input[type="submit"]'));
 
-  // assert user is redirected to '/'
-  // assert '/' is displayed properly
-  const tableRow = Selector("td")
-    .withText(username)
-    .parent();
-  await t
-    .expect(Selector("H1").withText("All Users").exists)
-    .ok()
-    .expect(tableRow.child().withText(username).exists)
-    .ok()
-    .expect(tableRow.child().withText(email).exists)
-    .ok()
-    .expect(Selector("a").withText("User Status").exists)
-    .ok()
-    .expect(Selector("a").withText("Log Out").exists)
-    .ok()
-    .expect(Selector("a").withText("Register").exists)
-    .notOk()
-    .expect(Selector("a").withText("Log In").exists)
-    .notOk();
+    // assert user is redirected to '/'
+    // assert '/' is displayed properly
+    const tableRow = Selector('td').withText(username).parent();
+    await t
+      .expect(Selector('H1').withText('All Users').exists).ok()
+      .expect(tableRow.child().withText(username).exists).ok()
+      .expect(tableRow.child().withText(email).exists).ok()
+      .expect(Selector('a').withText('User Status').exists).ok()
+      .expect(Selector('a').withText('Log Out').exists).ok()
+      .expect(Selector('a').withText('Register').exists).notOk()
+      .expect(Selector('a').withText('Log In').exists).notOk()
+      .expect(Selector('.alert-success').withText('Welcome!').exists).ok()
 
   // log a user out
   await t.click(Selector("a").withText("Log Out"));
@@ -104,3 +96,43 @@ test(`should validate the password field`, async (t) => {
     .expect(Selector('.validation-list > .error').nth(3).withText(
       'Password must be greater than 10 characters.').exists).ok()
 });
+
+test(`should throw an error if the credentials are incorrect`, async (t) => {
+  
+    // attempt to log in
+    await t
+      .navigateTo(`${TEST_URL}/login`)
+      .typeText('input[name="email"]', 'incorrect@email.com')
+      .typeText('input[name="password"]', password)
+      .click(Selector('input[type="submit"]'))
+  
+    // assert user login failed
+    await t
+      .expect(Selector('H1').withText('Login').exists).ok()
+      .expect(Selector('a').withText('User Status').exists).notOk()
+      .expect(Selector('a').withText('Log Out').exists).notOk()
+      .expect(Selector('a').withText('Register').exists).ok()
+      .expect(Selector('a').withText('Log In').exists).ok()
+      .expect(Selector('.alert-success').exists).notOk()
+      .expect(Selector('.alert-danger').withText(
+        'User does not exist.').exists).ok()
+  
+    // attempt to log in
+    await t
+      .navigateTo(`${TEST_URL}/login`)
+      .typeText('input[name="email"]', email)
+      .typeText('input[name="password"]', 'incorrectpassword')
+      .click(Selector('input[type="submit"]'))
+  
+    // assert user login failed
+    await t
+      .expect(Selector('H1').withText('Login').exists).ok()
+      .expect(Selector('a').withText('User Status').exists).notOk()
+      .expect(Selector('a').withText('Log Out').exists).notOk()
+      .expect(Selector('a').withText('Register').exists).ok()
+      .expect(Selector('a').withText('Log In').exists).ok()
+      .expect(Selector('.alert-success').exists).notOk()
+      .expect(Selector('.alert-danger').withText(
+        'User does not exist.').exists).ok()
+  
+  });
